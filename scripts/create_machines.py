0b2a0dd52be8d767ddcf30177dd8d76faa65b2b2
steam <steam1994@163.com>
2025-08-11 00:18:55 +0000

    update create machine

--- scripts/create_machines.py
+++ scripts/create_machines.py
@@ -22,45 +22,35 @@ if __name__ == '__main__':
     print("shards: ", num_shards)
     print("proxies: ", num_proxies)
 
-    # server_name_list = []
-    # for s in range(num_shards):
-    #     for r in range(num_replicas):
-    #         server_name = (tiga_common.TAG+"-server-s-"
-    #             +str(s).zfill(2)+"-r-"+str(r).zfill(2))
-    #         server_name_list.append(server_name)
-    # proxy_name_list = [ tiga_common.TAG+"-proxy-"+str(i).zfill(4) 
-    #                     for i in range(num_proxies) ]
+    server_name_list = []
+    for s in range(num_shards):
+        for r in range(num_replicas):
+            server_name = (tiga_common.TAG+"-server-s-"
+                +str(s).zfill(2)+"-r-"+str(r).zfill(2))
+            server_name_list.append(server_name)
+    proxy_name_list = [ tiga_common.TAG+"-proxy-"+str(i).zfill(4) 
+                        for i in range(num_proxies) ]
 
 
 
-    # for s in range(num_shards):
-    #     for r in range(num_replicas):
-    #         server_name = tiga_common.TAG+"-server-s-"+str(s).zfill(2)+"-r-"+str(r).zfill(2)
-    #         gcp_tools.create_instance(instance_name = server_name,
-    #                         image= IMAGE_FAMILY,
-    #                         machine_type =  server_vm_type,
-    #                         customzedZone=server_regions[r])
-    #         tiga_common.print_good(f"Created {server_name}")
+    for s in range(num_shards):
+        for r in range(num_replicas):
+            server_name = tiga_common.TAG+"-server-s-"+str(s).zfill(2)+"-r-"+str(r).zfill(2)
+            gcp_tools.create_instance(instance_name = server_name,
+                            image= tiga_common.IMAGE_FAMILY,
+                            machine_type =  tiga_common.SERVER_VM_TYPE,
+                            customzedZone=tiga_common.SERVER_REGIONS[r])
+            tiga_common.print_good(f"Created {server_name}")
     
-    # for i in range(num_proxies):
-    #     if i < 2 * len(server_regions): # local region
-    #         region = server_regions[i % len(server_regions)]
-    #     else:
-    #         region = proxy_regions[0]
-    #     gcp_tools.create_instance(instance_name = proxy_name_list[i],
-    #                     image= IMAGE_FAMILY,
-    #                     machine_type =  proxy_vm_type,
-    #                     customzedZone=region)
-    #     tiga_common.print_good(f"Created {proxy_name_list[i]}")
+    for i in range(num_proxies):
+        if i < 2 * len(tiga_common.SERVER_REGIONS): # local region
+            region = tiga_common.SERVER_REGIONS[i % len(tiga_common.SERVER_REGIONS)]
+        else:
+            region = tiga_common.REMOTE_REGIONS[0]
+        gcp_tools.create_instance(instance_name = proxy_name_list[i],
+                        image= tiga_common.IMAGE_FAMILY,
+                        machine_type =  tiga_common.PROXY_VM_TYPE,
+                        customzedZone=region)
+        tiga_common.print_good(f"Created {proxy_name_list[i]}")
         
-    # exit(0)
-
-
-    # for s in range(num_shards):
-    #     for r in range(num_replicas):
-    #         server_name = tiga_common.TAG+"-server-s-"+str(s).zfill(2)+"-r-"+str(r).zfill(2)
-    #         gcp_tools.modify_instance_list(
-    #             [server_name], 
-    #             machine_type="n2-standard-16",  
-    #             zone=tiga_common.SERVER_REGIONS[r])
-    #         tiga_common.print_good(f"Modified {server_name}")
+    exit(0)
